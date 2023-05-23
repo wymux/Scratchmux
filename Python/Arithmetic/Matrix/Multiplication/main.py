a08c4e756fcf421cc6a09de8dce8141ddf215b22
Edward Bates <gitmux@stemux.com>
2023-05-22 19:11:28 -0500

    matrix-multiplication::Python: Benchmark
    Signed-off-by: Edward Bates <gitmux@stemux.com>

--- Python/Arithmetic/Matrix/Multiplication/main.py
+++ Python/Arithmetic/Matrix/Multiplication/main.py
@@ -1,5 +1,6 @@
 from array import *
 import timeit
+import dis
 
 arr = [[3, 4], [1, 2]]
 arr1 = [[5, 4], [1, 3]]
@@ -12,6 +13,5 @@ def arr2():
     [((arr[0][1] * arr1[0][1]) + (arr[1][1] * arr1[1][1]))],
     ]
 
-duration = timeit.timeit(arr2, number=10000000)
+dis.dis(arr2)
 
-print(f"Time: {duration}\n")
