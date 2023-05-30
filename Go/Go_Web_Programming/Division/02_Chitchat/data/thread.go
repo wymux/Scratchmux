6fc3326ce8530c1b9d8504dd677c5765521257c7
Edward Bates <gitmux@stemux.com>
2023-05-30 16:30:01 -0500

    02_Chitchat: Inception
    Signed-off-by: Edward Bates <gitmux@stemux.com>

---
+++ Go/Go_Web_Programming/Division/02_Chitchat/data/thread.go
@@ -0,0 +1,121 @@
+package data
+
+import (
+	"time"
+)
+
+type Thread struct {
+	Id        int
+	Uuid      string
+	Body      string
+	Topic     string
+	UserId    int
+	ThreadId  int
+	CreatedAt time.Time
+}
+
+type Post struct {
+	Id        int
+	Uuid      string
+	Body      string
+	UserId    int
+	ThreadId  int
+	CreatedAt time.Time
+}
+
+func (thread *Thread) CreatedAtDate() string {
+	return thread.CreatedAt.Format("Jan 2, 2006 at 03:04 P.M.")
+}
+
+func (post *Post) CreatedAtDate() string {
+	return post.CreatedAt.Format("Jan 2, 2006 at 03:04 P.M.")
+}
+
+func (thread *Thread) NumReplies() (count int) {
+	rows, err := Db.Query("SELECT count(*) FROM posts where thread_id = $1", thread.Id)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		if err := rows.Scan(&count); err != nil {
+			return
+		}
+	}
+	rows.Close()
+	return
+}
+
+func (thread *Thread) Posts() (posts []Post, err error) {
+	rows, err := Db.Query("SELECT id, uuid, body, user_id, thread_id, created_at FROM posts where thread_id = $1", thread.Id)
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		post := Post{}
+		if err = rows.Scan(&post.Id, &post.Uuid, &post.UserId, &post.ThreadId, &post.CreatedAt); err != nil {
+			return
+		}
+		posts = append(posts, post)
+	}
+	rows.Close()
+	return
+}
+
+func (user *User) CreateThread(topic string) (conv Thread, err error) {
+	statement := "insert into threads (uuid, topic, user_id, created_at) values ($1, $2, $3, $4) returning id, uuid, topic, user_id, created_at"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(createUUID(), topic, user.Id, time.Now()).Scan(&conv.Id, &conv.Uuid, &conv.Topic, &conv.UserId, &conv.CreatedAt)
+	return
+}
+
+func (user *User) CreatePost(conv Thread, body string) (post Post, err error) {
+	statement := "insert into posts (uuid, body, user_id, thread_id, created_at) values ($1, $2, $3, $4, $5) returning id, uuid, body, user_id, thread_id, created_at"
+	stmt, err := Db.Prepare(statement)
+	if err != nil {
+		return
+	}
+	defer stmt.Close()
+	err = stmt.QueryRow(createUUID(), body, user.Id, conv.Id, time.Now()).Scan(&post.Id, &post.Uuid, &post.Body, &post.UserId, &post.ThreadId, &post.CreatedAt)
+	return
+}
+
+func Threads() (threads []Thread, err error) {
+	rows, err := Db.Query("SELECT id, uuid, topic, user_id, created_at FROM threads ORDER BY created_at DESC")
+	if err != nil {
+		return
+	}
+	for rows.Next() {
+		conv := Thread{}
+		if err = rows.Scan(&conv.Id, &conv.Uuid, &conv.Topic, &conv.UserId, &conv.CreatedAt); err != nil {
+			return
+		}
+		threads = append(threads, conv)
+	}
+	rows.Close()
+	return
+}
+
+func ThreadByUUID(uuid string) (conv Thread, err error) {
+	conv = Thread{}
+	err = Db.QueryRow("SELECT id, uuid, topic, user_id, created_at FROM threads WHERE uuid = $1", uuid).
+		Scan(&conv.Id, &conv.Uuid, &conv.Topic, &conv.UserId, &conv.CreatedAt)
+	return
+}
+
+func (thread *Thread) User() (user User) {
+	user = User{}
+	Db.QueryRow("SELECT id, uuid, name, email, created_at FROM users WHERE id = $1", thread.UserId).
+		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.CreatedAt)
+	return
+}
+
+func (post *Post) User() (user User) {
+	user = User{}
+	Db.QueryRow("SELECT id, uuid, name, email, created_at FROM users WHERE id = $1", post.UserId).
+		Scan(&user.Id, &user.Uuid, &user.Name, &user.Email, &user.CreatedAt)
+	return
+}
---
+++ Go/Go_Web_Programming/Division/02_Chitchat/route_auth.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"main/data"
+	"net/http"
+)
+
+func login(writer http.ResponseWriter, request *http.Request) {
+	t := parseTemplateFiles("login.layout", "public.navbar", "login")
+	t.Execute(writer, nil)
+}
+
+func signup(writer http.ResponseWriter, response *http.Request) {
+	generateHTML(writer, nil, "login.layout", "public.navbar", "signup")
+}
+
+func signupAccount(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	if err != nil {
+		danger(err, "Cannot parse form")
+	}
+	user := data.User{
+		Name:     request.PostFormValue("name"),
+		Email:    request.PostFormValue("email"),
+		Password: request.PostFormValue("password"),
+	}
+	if err := user.Create(); err != nil {
+		danger(err, "Cannot create user")
+	} else {
+		http.Redirect(writer, request, "/login", 302)
+	}
+}
+
+func authenticate(writer http.ResponseWriter, request *http.Request) {
+	err := request.ParseForm()
+	user, err := data.UserByEmail(request.PostFormValue("email"))
+	if err != nil {
+		danger(err, "Cannot find user")
+	}
+	if user.Password == data.Encrypt(request.PostFormValue("password")) {
+		session, err := user.CreateSession()
+		if err != nil {
+			danger(err, "Cannot create session")
+		}
+		cookie := http.Cookie{
+			Name:     "_cookie",
+			Value:    session.Uuid,
+			HttpOnly: true,
+		}
+		http.SetCookie(writer, &cookie)
+		http.Redirect(writer, request, "/", 302)
+	} else {
+		http.Redirect(writer, request, "/login", 302)
+	}
+}
+
+func logout(writer http.ResponseWriter, request *http.Request) {
+	cookie, err := request.Cookie("_cookie")
+	if err != http.ErrNoCookie {
+		warning(err, "Failed to get cookie.")
+		session := data.Session{Uuid: cookie.Value}
+		session.DeleteByUUID()
+	}
+	http.Redirect(writer, request, "/", 302)
+}
