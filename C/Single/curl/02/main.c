99bbd4aba856e5b6833a554b870b595d7b12f7ad
Lee Clark <wymux@shoash.com>
2023-09-22 22:45:51 +0000

    Curl/02[C]: Inception

---
+++ C/Single/curl/02/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <curl/curl.h>
+
+int
+main()
+{
+	CURL *curl;
+	CURLcode res;
+
+	curl = curl_easy_init();
+	if (curl) {
+		curl_easy_setopt(curl, CURLOPT_URL, "http://example.com");
+		curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+
+		res = curl_easy_perform(curl);
+		if (res != CURLE_OK) {
+			fprintf(stderr, "curl_esay_perform() failed: %s\n",
+				curl_easy_strerror(res));
+		}
+
+		curl_easy_cleanup(curl);
+	}
+	return 0;
+}
+	
